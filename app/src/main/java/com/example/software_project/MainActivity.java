db247f2b4fd711ad397db9c84d151d071ff10f3c
khkim <jyshoon@gmail.com>
2019-05-17 18:16:54 +0900

    commit test

--- app/src/main/java/com/example/software_project/MainActivity.java
+++ app/src/main/java/com/example/software_project/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -16,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d ("Test", "Test");
 
         //메인 화면
 
