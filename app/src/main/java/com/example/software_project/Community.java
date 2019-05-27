43e50af5e13d4a1ae07d61ba2ca95d1d7edce2cb
이형준 <49471710+HongZiZi@users.noreply.github.com>
2019-05-28 07:07:33 +0900

    웹뷰 적용완료

--- app/src/main/java/com/example/software_project/Community.java
+++ app/src/main/java/com/example/software_project/Community.java
@@ -2,20 +2,35 @@ package com.example.software_project;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.wifi.WifiEnterpriseConfig;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.webkit.WebViewClient;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 public class Community extends AppCompatActivity {
 
     private WebView webView;
     private Object imgbtn;
+
+    private long time= 0;
+    @Override
+    public void onBackPressed(){
+        if(System.currentTimeMillis()-time>=2000){
+            time=System.currentTimeMillis();
+            Toast.makeText(getApplicationContext(),"뒤로 버튼을 한번 더 누르면 종료합니다.",Toast.LENGTH_SHORT).show();
+        }else if(System.currentTimeMillis()-time<2000){
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +70,6 @@ public class Community extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent_cm = new Intent(getApplicationContext(), Community.class);
                 startActivity(intent_cm);
-                finish();
             }
         });
 
@@ -75,6 +89,16 @@ public class Community extends AppCompatActivity {
         // 웹뷰 액티비티
         webView = (WebView)findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setSupportZoom(false);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webView.getSettings().setAppCacheEnabled(false);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.setWebChromeClient(new WebChromeClient());
         webView.loadUrl("http://softwareblackcat.com/index.php?mid=comunnity");
     }
 }
